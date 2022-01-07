0270d71eb9c53b7b835a549b6c84633045314984
cj2001 <cj2001@gmail.com>
2022-01-07 15:02:25 -0700

    feat: updated queries

--- sql_queries/part19/2_degrees.sql
+++ sql_queries/part19/2_degrees.sql
@@ -1,3 +1,8 @@
+SELECT * FROM airports LIMIT 1;
+
+SELECT * FROM iroutes LIMIT 5;
+
+-- Find all of the airports within 2 connections of Denver, CO (DEN)
 WITH target AS (
     SELECT
         dest
@@ -19,4 +24,21 @@ WHERE
 
 ORDER BY
     dest
+;
+
+-- Count the number of airports within 2 connections of Denver, CO (DEN)
+WITH target AS (
+    SELECT
+        dest
+    FROM
+        iroutes
+    WHERE
+        src = 'DEN'
+)
+SELECT
+    COUNT(DISTINCT(i.dest))
+FROM
+    iroutes i
+WHERE
+    i.src IN (SELECT * FROM target)
 ;

24080a7593b5d431644a296361babf7b8ba801dd
cj2001 <cj2001@gmail.com>
2022-01-13 10:54:00 -0700

    feat: sql for 1 *OR* 2 hops

---
+++ sql_queries/part20/one_or_two_hops.sql
@@ -0,0 +1,33 @@
+-- Get all airports withing 1 *OR* 2 connections of Denver, CO (DEN)
+WITH target AS (
+    SELECT
+        dest
+    FROM
+        iroutes
+    WHERE
+        src = 'DEN'
+),
+two_hops AS (
+	SELECT
+		DISTINCT(i.dest)
+	FROM
+		iroutes i
+	WHERE
+		i.src IN (SELECT * FROM target)
+),
+both_hops AS (
+	SELECT 
+		a.dest 
+	FROM 
+		target a
+	UNION ALL
+	SELECT 
+		b.dest 
+	FROM 
+		two_hops b
+)
+SELECT
+	DISTINCT(dest)
+FROM
+	both_hops
+;
